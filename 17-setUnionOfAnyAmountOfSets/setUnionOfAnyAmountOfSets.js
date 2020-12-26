addcdd82dae0e12a0eb04775a90e564c8887291e
Hannah Williams <hannahdwilliams27@gmail.com>
2020-12-25 19:39:50 -0500

    Completed setUnionOfAnyAmountOfSets function

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,12 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  var s = new Set();
+  
+  for (let arg of args) {
+    for (let x of arg)
+      s.add(x);
+  }
+
+  return s;
 }
 
 // Do not edit this line;
